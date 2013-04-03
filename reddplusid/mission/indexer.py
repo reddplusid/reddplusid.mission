b75ccea8463c4dbdf8413a86c0c953d915841085
Khairil Yusof <kaeru@inigo-tech.com>
2013-04-03 13:10:54 +0800

    More robust full text indexer for missions. re #24

--- reddplusid/mission/indexer.py
+++ reddplusid/mission/indexer.py
@@ -9,35 +9,35 @@ from five import grok
 def index_searchable(obj):
 
     results = []
-    for attr in [
-                 'mission_location',
-                 ]:
-        results.append(' '.join(getattr(obj, attr, []) or []))
 
     results.append(obj.mission_funding_source)
     results.append(getattr(obj, 'output_stream', ''))
     results.append(getattr(obj, 'obj.mission_scope', ''))
     results.append(getattr (obj, 'obj.mission_funding_source', ''))
+
     if obj.id_province:
         results.append(id_provinces.getTerm(obj.id_province).title)
+
     if obj.country:
         results.append(p01.vocabulary.country.ISO3166Alpha2CountryVocabulary(obj).getTerm(obj.country).title)
+
     results.append(obj.output_contribution.output)
     results.append(obj.title)
     results.append(obj.description)
-      
+
     membership = obj.portal_membership
-    for memberId in obj.mission_author:
-        member = membership.getMemberById(memberId)
-        results.append(member.getProperty('fullname'))
 
-    for memberId in obj.mission_members:
-        member = membership.getMemberById(memberId)
-        results.append(member.getProperty('fullname'))
-
-    for memberId in obj.mission_support_staff:
-        member = membership.getMemberById(memberId)
-        results.append(member.getProperty('fullname'))
+    if obj.mission_members:
+        for memberId in obj.mission_members:
+            member = membership.getMemberById(memberId)
+            results.append(member.getProperty('fullname'))
+    
+   
+    if obj.mission_support_staff:
+        for memberId in obj.mission_support_staff:
+            member = membership.getMemberById(memberId)
+            results.append(member.getProperty('fullname'))
 
     return " ".join(results)
+
 grok.global_adapter(index_searchable, name='SearchableText')
