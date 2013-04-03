0ddec9ab704e149444bc5ef9e4fcffcf19d15f92
Khairil Yusof <kaeru@inigo-tech.com>
2013-04-03 14:40:04 +0800

    Add better check by content type for hint to user, whether a mission report has been added for a mission or not. re #67

--- reddplusid/mission/browser/mission_view.py
+++ reddplusid/mission/browser/mission_view.py
@@ -9,3 +9,21 @@ class Index(dexterity.DisplayForm):
     grok.require('zope2.View')
     grok.template('mission_view')
     grok.name('view')
+
+    
+    def contains_missionreport(self):
+        #Checks for existance of Mission Report in current Mission
+
+        context = self.context
+
+        cur_path = '/'.join(context.getPhysicalPath())
+        path = {}
+
+        path['query'] = cur_path
+        path['depth'] = 1
+
+        contentFilter = {}
+        contentFilter['path'] = path
+        contentFilter['portal_type'] = 'reddplusid.missionreport.missionreport'
+
+        return bool(context.portal_catalog.queryCatalog(contentFilter))
